747bf26dea48235938927828e987794402e3704d
Paulo Lopes Estevão <pl1745240@gmail.com>
2021-10-21 00:17:13 +0100

    add: loja

---
+++ database/factories/LojaFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Loja;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class LojaFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Loja::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
